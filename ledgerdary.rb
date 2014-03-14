56834b02c95043aee6e74c0eacaf07c23e7884ec
Cameron Hine <cam.hine@rea-group.com>
2014-03-14 18:05:29 +1100

    Add help message.

---
+++ ledgerdary.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+def help
+  <<END
+useage: ledgerdary CSV_FILE LEDGER_FILE
+
+Ledgerdary merges/reconciles ledger data files with transaction
+information provided by financial institutions. Account transactions
+described by CSV file are merged with ledger data files producting a
+new ledger data file contatining untracked expenses from the CSV.
+END
+end
+
+puts help
